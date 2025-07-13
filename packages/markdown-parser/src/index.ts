be4a7d42a194eb770c2e804882c8684036541eb9
juwenzhang <3137536916@qq.com>
2025-07-13 23:17:35 +0800

    feat: markdown-parser first commit

---
+++ packages/markdown-parser/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./types/TokenType"
+export * from "./utils/Tokenizer"
+export * from "./utils/parser"
+export * from "./utils/html"
---
+++ packages/markdown-parser/src/types/TokenType.ts
@@ -0,0 +1,32 @@
+export type TokenTypes =
+  | 'heading'
+  | 'paragraph'
+  | 'list'
+  | 'listItem'
+  | 'codeBlock'
+  | 'link'
+  | 'image'
+  | 'text'
+  | 'emphasis'
+  | 'strong'
+  | 'inlineCode'
+  | 'html'
+  | 'thematicBreak'
+  | 'document'
+
+export interface Token {
+  type: TokenTypes,
+  value?: string,
+  level?: number,
+  ordered?: boolean,
+  children?: Token[],
+  href?: string,
+  alt?: string,
+  lang?: string,
+  title?: string,
+}
+
+export interface ParserState {
+  input: string;
+  position: number;
+}
